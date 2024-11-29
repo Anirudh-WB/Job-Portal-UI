44b81018ec6a69266d4716573030ea9bd86bcdb1
Nimesh Jethva <nimesh.jethva@wonderbiz.in>
2024-11-29 10:42:15 +0530

    add react select

--- src/pages/job/JobPost/PreferredCityPage.tsx
+++ src/pages/job/JobPost/PreferredCityPage.tsx
@@ -12,6 +12,8 @@ import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import JobCityUtility from "../../../utilities/job/JobCityUtility";
 import SaveIcon from "@mui/icons-material/Save";
+import CityModel from "../../../model/master/CityModel";
+import Select from "react-select";
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
@@ -31,7 +33,7 @@ const PreferredCityPage: React.FC<{ parentJobId: number }> = ({
         </div>
 
         {/* Autocomplete Input Section */}
-        <div className="flex flex-col gap-1 w-full">
+        {/* <div className="flex flex-col gap-1 w-full">
           <Autocomplete
             multiple
             id="checkboxes-tags-demo"
@@ -61,7 +63,40 @@ const PreferredCityPage: React.FC<{ parentJobId: number }> = ({
               />
             )}
           />
-        </div>
+        </div> */}
+
+        <div className="flex flex-col gap-1 w-full">
+        <Select
+          id="skills"
+          isMulti
+          options={utility.jobCities}
+          closeMenuOnSelect={false}
+          value={utility.selectedCities}
+          getOptionLabel={(option: CityModel) => option.cityName}
+          isOptionSelected={(option: CityModel, value: readonly CityModel[]) => {
+            return value.some((selectedOption) => selectedOption.id === option.id);
+          }}
+          onChange={utility.onCityChange}
+          placeholder="Add Location"
+          className="w-full"
+          styles={{
+            control: (base) => ({
+              ...base,
+              borderColor: "rgb(209, 213, 219)", // Tailwind gray-300
+              boxShadow: "none",
+              "&:hover": { borderColor: "rgb(156, 163, 175)" }, // Tailwind gray-500
+              padding: "5px",
+            }),
+            option: (base, { isSelected }) => ({
+              ...base,
+              backgroundColor: isSelected ? "rgb(96, 165, 250)" : "white", // Tailwind blue-500
+              color: isSelected ? "white" : "black",
+              cursor: "pointer",
+              borderRadius: isSelected ? "5px" : "10px"
+            }),
+          }}
+        />
+      </div>
 
         {/* Save Button */}
         <div className="text-right">
@@ -70,7 +105,7 @@ const PreferredCityPage: React.FC<{ parentJobId: number }> = ({
             className="bg-blue-700 text-white font-semibold py-2 px-8 rounded-3xl"
             onClick={utility.onJobCitySave}
           >
-            Save
+            Apply
           </button>
         </div>
       </div>
