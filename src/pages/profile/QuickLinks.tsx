d58e3874055a64a8e998c3d3cf88b826751361f5
Anirudh Gupta <anirudh.gupta@wonderbiz.in>
2024-12-05 14:38:26 +0530

    changes

--- src/pages/profile/QuickLinks.tsx
+++ src/pages/profile/QuickLinks.tsx
@@ -7,26 +7,17 @@ function QuickLinks({}: Props) {
     <div className="flex flex-col gap-7 bg-white rounded-xl border p-5 w-1/4 h-fit sticky top-0">
       <h2 className="font-semibold text-lg">Quick Links</h2>
       <div className="flex flex-col px-2 gap-7 text-sm font-medium">
-        <a href="#Resume" className="flex items-center justify-between">
-          Resume {/* {hasAccess && ( */}
-          <span className="font-semibold text-blue-700">Update</span>
-          {/* )} */}
+        <a href="#academinc-info" className="flex items-center justify-between">
+          Academinc Info
         </a>
-        <a href="#Resume-headline">Resume headline</a>
-        <a href="#Employment" className="flex items-center justify-between">
-          Employment {/* {hasAccess && ( */}
-          <span className="font-semibold text-blue-700">Add</span>
-          {/* )} */}
+        <a
+          href="#employment-info"
+          className="flex items-center justify-between"
+        >
+          Employment Info
         </a>
-        <a href="#Education" className="flex items-center justify-between">
-          Education {/* {hasAccess && ( */}
-          <span className="font-semibold text-blue-700">Add</span>
-          {/* )} */}
-        </a>
-        <a href="#IT-skills">IT skills</a>
-        <a href="#Projects">Projects</a>
-        <a href="#Career-profile">Career profile</a>
-        <a href="#Personal-details">Personal details</a>
+        <a href="#skill-info">Skill Info</a>
+        {/* <a href="#Projects">Projects</a> */}
       </div>
     </div>
   );
